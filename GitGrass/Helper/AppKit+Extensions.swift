8f5c00292783adcad9736ab0584e81cf91d6b6cd
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2023-01-27 01:51:36 +0900

    Copy&Paste for TextField

--- GitGrass/Helper/AppKit+Extensions.swift
+++ GitGrass/Helper/AppKit+Extensions.swift
@@ -115,3 +115,26 @@ extension NSImage {
         self.isTemplate = (color == .monochrome)
     }
 }
+
+extension NSTextField {
+    override open func performKeyEquivalent(with event: NSEvent) -> Bool {
+        let flags = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+        if flags == [.command] {
+            let selector: Selector
+            switch event.charactersIgnoringModifiers?.lowercased() {
+            case "x": selector = #selector(NSText.cut(_:))
+            case "c": selector = #selector(NSText.copy(_:))
+            case "v": selector = #selector(NSText.paste(_:))
+            case "a": selector = #selector(NSText.selectAll(_:))
+            case "z": selector = Selector(("undo:"))
+            default: return super.performKeyEquivalent(with: event)
+            }
+            return NSApp.sendAction(selector, to: nil, from: self)
+        } else if flags == [.shift, .command] {
+            if event.charactersIgnoringModifiers?.lowercased() == "z" {
+                return NSApp.sendAction(Selector(("redo:")), to: nil, from: self)
+            }
+        }
+        return super.performKeyEquivalent(with: event)
+    }
+}
